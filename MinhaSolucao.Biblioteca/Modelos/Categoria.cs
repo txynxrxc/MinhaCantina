7e735d02ac4e80d682de7af68eae674e41d5fca8
Gaaldra <55445484+Gaaldra@users.noreply.github.com>
2025-12-02 15:28:19 -0300

    Adicionar arquivos de projeto.

---
+++ MinhaSolucao.Biblioteca/Modelos/Categoria.cs
@@ -0,0 +1,53 @@
+﻿namespace MinhaSolucao.Biblioteca.Modelos;
+
+public class Categoria
+{
+	// Sintaxe de Propriedade:
+	// <modificador_acesso> <tipo_dado> <NomePropriedade>
+	// Modificadores de acesso: public, internal e private
+	// Tipos de dados: string, int, float/decimal/ bool
+	public int Id { get; private set; }
+	public string Nome { get; private set; }
+
+	// Sintaxe de Construtor:
+	// <modificador_acesso> <NomeClasse>
+	
+	// Serve para o ORM
+	private Categoria() { }
+
+	// Criaremos um metodo de fabrica chamado Criar();
+	public static Categoria Criar(string nomeCategoria)
+	{
+		// Crie uma verificação para ver se o nomeCategoria é nulo ou tem espaços em branco, se tiver, jogue (throw) um novo (new) objeto do tipo Exception com a mensagem: "Nome da categoria não pode ser nulo ou vazio"
+		if (string.IsNullOrWhiteSpace(nomeCategoria))
+		{
+			throw new Exception("Nome da categoria não pode ser nulo ou vazio");
+		}
+		else
+		{
+			return new Categoria()
+			{
+				Nome = nomeCategoria
+			};
+		}
+	}
+
+	// Criaremos um metodo para modificar o nome da categoria
+	// <modificador> <retorno> <NomeMetodo>(parametros){
+	//	Ações:
+	//  1º Se o novoNome (parametro) é nulo ou vazio, se for, joga um novo objeto Exception com os dizeres: "O novo nome da categoria não pode ser nulo ou vazio"
+	//  2º Acessar a propriedade Nome e passar para ela o novoNome
+	// }
+	public void MudarNome(string novoNome)
+	{
+		if (string.IsNullOrWhiteSpace(novoNome))
+		{
+			throw new Exception("O novo nome da categoria não pode ser nulo ou vazio");
+		}
+		else
+		{
+			// Acessa a propriedade Nome e aplica nela a informação de novoNome
+			this.Nome = novoNome;
+		}
+	}
+}
---
+++ MinhaSolucao.Biblioteca/Modelos/Produto.cs
@@ -0,0 +1,73 @@
+﻿namespace MinhaSolucao.Biblioteca.Modelos;
+
+public class Produto
+{
+	public int Id { get; private set; }
+	public string Nome { get; private set; }
+	public decimal Preco { get; private set; }
+	public string? Descricao { get; private set; }
+	public Categoria Categoria { get; private set; }
+
+	// Construtor
+	private Produto() { }
+
+	// Metodo estatico de criação chamado Criar()
+	// Parametros: nomeProduto, precoProduto, categoriaProduto
+	// <modificador> <static> <retorno> <NomeMetodo>(<parametros>){
+	// Ações: 1º Verificar se o parametro nomeProduto é vazio ou nulo
+	// 2º Verificar se o precoProduto é negativo
+	//}
+
+	public static Produto Criar(string nomeProduto, decimal precoProduto, Categoria categoriaProduto)
+	{
+		if (string.IsNullOrWhiteSpace(nomeProduto))
+		{
+			throw new Exception("O nome do produto não pode ser nulo ou vazio");
+		}
+		else if (decimal.IsNegative(precoProduto))
+		{
+			throw new Exception("O preço do produto não pode ser abaixo de R$ 0");
+		}
+		else
+		{
+			return new Produto()
+			{
+				Nome = nomeProduto,
+				Preco = precoProduto,
+				Categoria = categoriaProduto
+			};
+		}
+	}
+
+	// Metodo que altere o nome do produto
+	public void MudarNome(string novoNome)
+	{
+		if (string.IsNullOrWhiteSpace(novoNome))
+		{
+			throw new Exception("O novo nome do produto não pode ser nulo ou vazio");
+		}
+		else
+		{
+			this.Nome = novoNome;
+		}
+	}
+
+	// Metodo que altere o preço do produto
+	public void MudarPreco(decimal novoPreco)
+	{
+		if (decimal.IsNegative(novoPreco))
+		{
+			throw new Exception("O novo preço não pode ser negativo");
+		}
+		else
+		{
+			this.Preco = novoPreco;
+		}
+	}
+
+	// Metodo que altere a categoria do produto
+	public void MudarCategoria(Categoria novaCategoria)
+	{
+		this.Categoria = novaCategoria;
+	}
+}
